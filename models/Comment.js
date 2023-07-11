71dac4e35316359408451757c77972500987b664
JanicaJensen <122334098+JanicaJensen@users.noreply.github.com>
2023-07-11 08:16:47 -0600

    added comment routes to /api

--- models/Comment.js
+++ models/Comment.js
@@ -5,30 +5,8 @@ class Comment extends Model { }
 
 Comment.init(
     {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
         comment_text: {
             type: DataTypes.STRING,
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'user',
-                key: 'id',
-            },
-        },
-        post_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'post',
-                key: 'id',
-            },
         },
     },
     {
